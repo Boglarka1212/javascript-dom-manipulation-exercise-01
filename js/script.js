164e7c8ca6e44b8fa3753f0241df3637fae39b06
Boglarka1212 <kazmer.boglarka@gmail.com>
2026-02-24 13:28:47 +0100

    fontsize og weight ændret på p

--- js/script.js
+++ js/script.js
@@ -3,9 +3,12 @@
 const websiteBody = document.body;
 
 const headingOne = document.querySelector("h1");
+const paragraph = document.querySelector("p");
 
 websiteBody.style.background = "pink";
 
 headingOne.style.fontSize = "3rem";
 headingOne.style.color = "Maroon";
 
+paragraph.style.fontSize = "2rem";
+paragraph.style.fontWeight = "700";
