348a5ad5366f1cdb55b7575a64a966eedf409116
Boglarka1212 <kazmer.boglarka@gmail.com>
2026-02-24 13:32:50 +0100

    list items oprettet som variabel

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const websiteBody = document.body;
 const headingOne = document.querySelector("h1");
 const paragraph = document.querySelector("p");
 
+const listItemOne = document.querySelector("#list-item-one");
+const listItemTwo = document.querySelector("#list-item-two");
+const listItemThree = document.querySelector("#list-item-three");
+
+
 websiteBody.style.background = "pink";
 
 headingOne.style.fontSize = "3rem";
